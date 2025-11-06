b00a05b5ccbed6c0cb8a4e27034af32da1720606
Kirandeep793 <kaurkirandeep369@gmail.com>
2025-11-06 14:59:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+var indexRouter = require('./routes/index');
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.status(404).send('Page not found');
+});
+
+module.exports = app;
