6c1799c8280536a694b135b28539956a90e6a9c5
Kirandeep793 <kaurkirandeep369@gmail.com>
2025-11-06 15:40:00 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const port = process.env.PORT || 8080;
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve homepage message
+app.get('/', (req, res) => {
+  res.send('My Student ID is 22094629. Welcome to Express!');
+});
+
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
 });
+
